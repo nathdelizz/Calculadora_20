bb48fbbed27b93071319f12c0ab99ce61c96075a
ifpr <nathaliadeliz3@gmail.com>
2023-04-12 14:54:47 -0300

    Fiz o botão de voltar

--- app/src/main/java/com/example/calculadora20/ResultadoCalculadora.java
+++ app/src/main/java/com/example/calculadora20/ResultadoCalculadora.java
@@ -4,14 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ResultadoCalculadora extends AppCompatActivity {
-
+Button buttonVoltar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado_calculadora);
+        buttonVoltar = findViewById(R.id.buttonVoltar);
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(ResultadoCalculadora.this, MainActivity.class);
+                startActivity(in);
+            }
+        });
+
         Intent it = getIntent();
 
         String PrimeiroValor = it.getStringExtra("PrimeiroValor");
